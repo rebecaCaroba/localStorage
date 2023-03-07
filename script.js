be3c2e921d703aa2e7bda59c1dda36af3c5f9729
rebecaCaroba <rebecamartin200@gmail.com>
2023-03-07 09:56:49 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const addBtn = document.querySelector('#add');
-const clearBtn = document.querySelector('#clear');
-const form = document.querySelector('#form-name');
-const texts = [] 
+let addBtn = document.querySelector('#add');
+let clearBtn = document.querySelector('#clear');
+let form = document.querySelector('#form-name');
+let res = document.querySelector('#res');
+let texts = [];
 
 addBtn.addEventListener('click', () => {
-    const text = document.querySelector('#text').value;
-    texts.push(text);
-    
-    localStorage.setItem('texts', JSON.stringify(texts));
-    const getTexts = localStorage.getItem('texts');
+    let newText = document.querySelector('#text').value;
+    texts.push(newText);
+
+    localStorage.setItem('artexts', JSON.stringify(texts));
+    let getTexts = localStorage.getItem('artexts');
     console.log(getTexts);
+    document.querySelector('#text').value = ' ';
+});
+
+clearBtn.addEventListener('click', () => {
+    texts = [];
+    localStorage.artexts = JSON.stringify(texts);
+    if(res.length != ' '){
+        res.innerHTML = 'Texto limpado..'
+    }
 });
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    res.innerHTML = ''
+    if (localStorage.artexts) {
+        texts = JSON.parse(localStorage.getItem('artexts'));
+    }
+
+    for (let i in texts) {
+    let p = document.createElement('p');
+        p.innerText += `${texts[i]}`
+        res.append(p)
+    }
 });
