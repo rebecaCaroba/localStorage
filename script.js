ae90fcb36b35fc9a5295ca0fcc65b213afc6a66f
rebecaCaroba <rebecamartin200@gmail.com>
2023-04-18 16:28:18 -0300

    initial commit

--- script.js
+++ script.js
@@ -12,25 +12,33 @@ addBtn.addEventListener('click', () => {
     let getTexts = localStorage.getItem('artexts');
     console.log(getTexts);
     document.querySelector('#text').value = ' ';
+    document.querySelector('#text').focus();
+
 });
 
 clearBtn.addEventListener('click', () => {
     texts = [];
     localStorage.artexts = JSON.stringify(texts);
-    if(res.length != ' '){
+    if (res.length != ' ') {
         res.innerHTML = 'Texto limpado..'
     }
 });
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    res.innerHTML = ''
-    if (localStorage.artexts) {
+    res.innerHTML = '';
+
+    if (localStorage.getItem('artexts') !== null && JSON.parse(localStorage.getItem('artexts')).length === 0) {
+
+        res.innerText = 'Não exite valores adicionados..'
+    } else {
         texts = JSON.parse(localStorage.getItem('artexts'));
     }
 
+
+
     for (let i in texts) {
-    let p = document.createElement('p');
+        let p = document.createElement('p');
         p.innerText += `${texts[i]}`
         res.append(p)
     }
